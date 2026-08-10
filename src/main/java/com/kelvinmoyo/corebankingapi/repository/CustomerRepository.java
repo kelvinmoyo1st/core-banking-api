d927cfdbeacacb8c5abb11e669b845c6acc7ff03
kelvinmoyo1st <kelvinmoyo1st@gmail.com>
2026-08-10 10:18:03 +0000

    Add CustomerRepository, AccountRepository, TransactionRepository - verified Spring Data JPA scans and validates all 3 interfaces at boot

---
+++ src/main/java/com/kelvinmoyo/corebankingapi/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.kelvinmoyo.corebankingapi.repository;
+
+import com.kelvinmoyo.corebankingapi.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
---
+++ src/main/java/com/kelvinmoyo/corebankingapi/repository/TransactionRepository.java
@@ -0,0 +1,11 @@
+package com.kelvinmoyo.corebankingapi.repository;
+
+import com.kelvinmoyo.corebankingapi.entity.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+
+    List<Transaction> findByAccountId(Long accountId);
+}
